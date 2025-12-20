77efef7df4de886013fe29cd84728968d1319f98
zukash <48586209+zukash@users.noreply.github.com>
2025-12-20 09:26:26 +0900

    Update notif to v1.1.2

--- Formula/notif.rb
+++ Formula/notif.rb
@@ -1,8 +1,8 @@
 class Notif < Formula
   desc "Minimal macOS Notification Center controller for keyboard shortcuts"
   homepage "https://github.com/zukash/notif"
-  url "https://github.com/zukash/notif/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "8d96139c5b04165c7aa7a39e4b07659a219bf8b7e2a802fe1199558a899769f9"
+  url "https://github.com/zukash/notif/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "83c38a9aec2ebbcc23f28b8332909293d9d3ef79d9a8fcf6650c8d63d9f07516"
   license "MIT"
 
   def install
